4509cf1d7feb5b60b9d5f8383410a0ccd7fe26e7
VRBlue - ice tag Creator <vrblue@proton.me>
2026-03-22 10:52:25 -0500

    27.1 HERE I COMEEEEEE

---
+++ xcode/macOS/ice%20tag/build/Build/Intermediates.noindex/ice tag.build/Debug/ice tag.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "Iceberg-waters-Antarctica Background Removed" asset catalog image resource.
+static NSString * const ACImageNameIcebergWatersAntarcticaBackgroundRemoved AC_SWIFT_PRIVATE = @"Iceberg-waters-Antarctica Background Removed";
+
+/// The "Untitled" asset catalog image resource.
+static NSString * const ACImageNameUntitled AC_SWIFT_PRIVATE = @"Untitled";
+
+#undef AC_SWIFT_PRIVATE
